38b3bc8c9ecede1bcdec317380358957a99cbb82
Rachel Luginbill <rachelluginbill27@gmail.com>
2026-02-23 10:23:26 -0800

    Settings menu complete!

--- Project3/MainMenu.cpp
+++ Project3/MainMenu.cpp
@@ -1,12 +1,14 @@
 /*Name:Rachel Luginbll
-Last Edited: Febuary 9, 2026
-Version: 1.0
+Last Edited: Febuary 23, 2026
+Version: 2.0
 File Name: Main_Menu.cpp
+
 Description: this is the file for the main menu select. After text is displayed user will be able to enter
 one of four different values which will then be checked and bring the user to one of four screens. First N will start
 a new game jumping to a different file called Story_Set_Up, L will load the game from where the user last saved,
 S will bring the user to the settings menu, and E will display a prompt to users 'are you sure you wish to exit?'
-All pictures to come later with microservice*/
+All pictures to come later with microservice
+*/
 
 
 using namespace std;
--- Project3/Settings.cpp
+++ Project3/Settings.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-char usersDecision;
+char usersDesgVal;
 
 void SettingsMenu()
 {
@@ -30,21 +30,79 @@ void SettingsMenu()
 
 	//ask user if they would like to change any of the keys or set to default values
 		cout << "Would you like to change any of the key options? (Y/N)" << endl;
-		cin >> usersDecision;
-		usersDecision = toupper(usersDecision);
+		cin >> usersDesgVal;
+        usersDesgVal = toupper(usersDesgVal);
 
-		if(usersDecision != 'Y' && usersDecision != 'N')
-		{
-			cout << "Invalid entry. Please enter Y or N." << endl;
-			cin >> usersDecision;
-			usersDecision = toupper(usersDecision);
-		}
+        do {
+            cout << "Enter Y or N: ";
+            cin >> usersDesgVal;
+            usersDesgVal = toupper(usersDesgVal);
+
+            if (usersDesgVal != 'Y' && usersDesgVal != 'N') {
+                cout << "Invalid entry. Please enter Y or N." << endl;
+            }
+
+        } while (usersDesgVal != 'Y' && usersDesgVal != 'N');
 	//if yes, cycle through each key and ask user if they would like to change it, if yes, ask for new value and validate it
-		else if (usersDecision == 'Y') {
+        if (usersDesgVal == 'Y')
+        {
+            char* keys[] = {
+                &option1, &option2,
+                &mainMenu, &settingsMenu, &trophiesMenu,
+                &newGameOption, &loadGameOption,
+                &exitGameOption, &saveGameOption
+            };
 
-		}
+            const char* keyNames[] = {
+                "Option 1", "Option 2",
+                "Main Menu", "Settings Menu", "Trophies Menu",
+                "New Game", "Load Game",
+                "Exit Game", "Save Game"
+            };
+
+            const int numKeys = 9;
+
+            for (int i = 0; i < numKeys; i++)
+            {
+                char changeChoice;
+
+                cout << "\n" << keyNames[i]
+                    << " (current: " << *keys[i] << ")";
+                cout << "\nChange this key? (Y/N): ";
+
+                cin >> changeChoice;
+                changeChoice = toupper(changeChoice);
+
+                while (changeChoice != 'Y' && changeChoice != 'N')
+                {
+                    cout << "Invalid entry. Enter Y or N: ";
+                    cin >> changeChoice;
+                    changeChoice = toupper(changeChoice);
+                }
+
+                if (changeChoice == 'Y')
+                {
+                    char newValue;
+
+                    cout << "Enter new character value: ";
+                    cin >> newValue;
+                    newValue = toupper(newValue);
+
+                    // Optional validation: letters only
+                    while (newValue < 'A' || newValue > 'Z')
+                    {
+                        cout << "Invalid entry. Enter A-Z: ";
+                        cin >> newValue;
+                        newValue = toupper(newValue);
+                    }
+
+                    *keys[i] = newValue;
+                    cout << "Updated successfully.\n";
+                }
+            }
+        }
 	//if no, return to main menu
-		else if(usersDecision == 'N')
+		else if(usersDesgVal == 'N')
 		{
 			main();
 		}
